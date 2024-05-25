83dd0897b7101051a64e0e6ce11167d1fad31797
Jip Steiger <jipsteiger95@gmail.com>
2024-05-25 16:24:16 +0200

    RTC jip 56

--- RTC/heuristic_rules.py
+++ RTC/heuristic_rules.py
@@ -22,12 +22,15 @@ def p10_1(nodes, target=None):
     return 'p10_1', target 
 
 def p_21_2(nodes, target=None):
-    # if (nodes["j_2"].depth >= 0.20) & (nodes["j_21"].depth <= 0.15):
-    #     target = 0
     if nodes["j_21"].depth >= 0.25:
-        target = 1
+        if (nodes['j_1'].depth <= 0.5):
+            target = 1
+        elif (nodes['j_1'].depth >= 0.5) & (nodes['j_21'].depth >= 1):
+            target = 1
+        else: 
+            target = 0
     if nodes["j_21"].depth <= 0.10:
-        target = 0    
+        target = 0     
     return 'p_21_2', target
     
 def WWTP_inlet(nodes, target=None):
