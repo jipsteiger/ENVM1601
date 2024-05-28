134bdad33290ad815dc8b4f303f96e489c22e3aa
Jip Steiger <j.r.steiger@student.tudelft.nl>
2024-05-28 15:20:31 +0200

    RTC and Failure

--- RTC/event_optimisation copy.py
+++ RTC/event_optimisation copy.py
@@ -2,8 +2,10 @@ import pyswmm
 import pandas as pd
 import os
 import pulp as pl
-import swmm_api
+import swmm_api as sa
 import numpy as np
+from RTC.heuristic_rules_simulation import process_output
+import datetime as dt
 
 
 NUMBER_OF_TIME_STEPS = 3  # Number of time steps that are used for prediction
@@ -16,11 +18,32 @@ P_21_2_MAX = 0.0417  # CMS
 CSO_PUMP_2_MAX = 0.5556  # CMS
 CSO_PUMP_21_MAX = 0.0278  # CMS
 
+EVENT_NAME = [
+    "Major event june",
+    "Minor event december",
+    "Multiple average events september",
+    "3 major events february",
+    "Peak intensity event february",
+]
+
+CSOS = {
+    "cso_1": 0,
+    "cso_20": 0,
+    "cso_2a": 0,
+    "cso_21a": 0,
+    "cso_10": 0,
+    "cso_21b": 0,
+    "cso_2b": 0,
+}
+
 CSO_CREST_HEIGHT = [1.83, 2.21, 2.48, 1.90, 2.16]  # Crest height of all weirs
 
 junctions = ["j_1", "j_10", "j_2", "j_20", "j_21"]
 
 JUNCTION_MAX_STORAGE = {}
+
+som = 0
+cso_sum = 0
 
 # Calculate max storage in node based on weir crest height by using the storage curve
 for i, junction in enumerate(junctions):
@@ -37,7 +60,7 @@ for i, junction in enumerate(junctions):
     JUNCTION_MAX_STORAGE[junction] = max_storage
 
 for file_number in range(1, 5 + 1):
-    output = swmm_api.read_out_file(
+    output = sa.read_out_file(
         rf"RTC\event_optimisation_output_data\Dean Town_pyswmm_{file_number}.out"
     ).to_frame()
     j1_in = output["node"]["j_1"]["lateral_inflow"]
@@ -91,7 +114,7 @@ for file_number in range(1, 5 + 1):
                 # Add boundaries for each timestep
                 for i in range(0, NUMBER_OF_TIME_STEPS):
 
-                    # Setup objective function
+                    # Setup objective function values
                     if file_number == 1:  # Only file number 1 is during bathing season
                         for j, weight in enumerate([2, 2, 2, 2, 2, 1 / 500, 1 / 500]):
                             spill_obj += (
@@ -102,7 +125,6 @@ for file_number in range(1, 5 + 1):
                             spill_obj += (
                                 x_vars[j + 1 + 12 * i] * weight
                             )  # for each CSO (j+1) add weight, per timestep (+ 12 * i)
-                    prob += equal_fill_obj + spill_obj
 
                     # Add CSO boundary condition
                     prob += x_vars[1 + 12 * i] >= 0
@@ -146,6 +168,9 @@ for file_number in range(1, 5 + 1):
                     )
                     reservoir_delta_j_20 += -x_vars[4 + 12 * i] - x_vars[10 + 12 * i]
                     reservoir_delta_j_21 += -x_vars[5 + 12 * i] - x_vars[7 + 12 * i]
+
+                # Set FUNCTION OBJECTIVE
+                prob += equal_fill_obj + spill_obj
 
                 # RESERVOIR VOLUMES
                 # current volume + inflow - outflow cannot be more than the max volume in node
@@ -208,19 +233,84 @@ for file_number in range(1, 5 + 1):
                 ) <= JUNCTION_MAX_STORAGE["j_21"]
 
                 prob.solve()
-                print(prob.variables())
-                break
 
-        break
-    break
-    # Update pump controls
-    # links["CSO_Pump_2"].target_setting = prob.variables()[32].varValue / CSO_PUMP_2_MAX
-    # links["CSO_Pump_21"].target_setting = prob.variables()[33].varValue / CSO_PUMP_21_MAX
-    # links["p10_1"].target_setting = prob.variables()[34].varValue / P_10_1_MAX
-    # links["p_2_1"].target_setting = prob.variables()[35].varValue / P_2_1_MAX
-    # links["p_20_2"].target_setting = prob.variables()[1].varValue / P_20_2_MAX
-    # links["p_21_2"].target_setting = prob.variables()[2].varValue / P_21_2_MAX
-    # links["WWTP_inlet"].target_setting = prob.variables()[3].varValue / WWTP_INLET_MAX
+                # Update pump controls
+                links["CSO_Pump_2"].target_setting = (
+                    prob.variables()[32].varValue / CSO_PUMP_2_MAX
+                )
+                links["CSO_Pump_21"].target_setting = (
+                    prob.variables()[33].varValue / CSO_PUMP_21_MAX
+                )
+                links["p10_1"].target_setting = (
+                    prob.variables()[34].varValue / P_10_1_MAX
+                )
+                links["p_2_1"].target_setting = (
+                    prob.variables()[35].varValue / P_2_1_MAX
+                )
+                links["p_20_2"].target_setting = (
+                    prob.variables()[1].varValue / P_20_2_MAX
+                )
+                links["p_21_2"].target_setting = (
+                    prob.variables()[2].varValue / P_21_2_MAX
+                )
+                links["WWTP_inlet"].target_setting = (
+                    prob.variables()[3].varValue / WWTP_INLET_MAX
+                )
+
+    # ----------------------------------------------------------------------------------
+    # Process output
+    rpt = sa.read_rpt_file(
+        rf"RTC\event_optimisation_input_data\Dean Town_pyswmm_{file_number}.rpt"
+    )
+    output = rpt.outfall_loading_summary
+    output = output.drop(["Wastewater_Treatment_Plant"], axis=0)
+    flooding = rpt.flow_routing_continuity
+    initial_sum = som
+
+    print(EVENT_NAME[file_number])
+    print(f"Total CSO overflow in this event: {output['Total_Volume_10^6 ltr'].sum()}")
+    summer = [2, 2, 2, 2, 2, 1 / 500, 1 / 500]
+    winter = [1, 1, 1, 1, 2, 1 / 500, 1 / 500]
+
+    for i, cso in enumerate(
+        ["cso_1", "cso_20", "cso_2a", "cso_21a", "cso_10", "cso_21b", "cso_2b"]
+    ):
+        if EVENT_NAME[file_number] == "Major event june":
+            som += (
+                output.loc[cso, "Total_Volume_10^6 ltr"] * summer[i]
+            )  # For goss river recrreation
+            CSOS[cso] += output.loc[cso, "Total_Volume_10^6 ltr"] * summer[i]
+        else:
+            som += output.loc[cso, "Total_Volume_10^6 ltr"] * winter[i]  # No recreation
+            CSOS[cso] += output.loc[cso, "Total_Volume_10^6 ltr"] * winter[i]
+        print(f'{cso} spilled: {output.loc[cso, "Total_Volume_10^6 ltr"]:.3f}')
+        cso_sum += output.loc[cso, "Total_Volume_10^6 ltr"]
+    som += flooding["Flooding Loss"]["Volume_10^6 ltr"] * 10000  # Flooding addition
+    print(f"Flooding loss is: {flooding['Flooding Loss']['Volume_10^6 ltr']:.3f}")
+    print(f"Event objective function contribution: {som - initial_sum} ")
+
+    print(f"Objective function end result: {som}")
+    print(f"Objecttive function contribition per CSO:")
+
+    now = dt.datetime.now()
+    cso_result = pd.DataFrame(CSOS, index=[now.strftime("%d/%m %H:%M")])
+    sum_result = pd.DataFrame(
+        {"obj_fun_sum": som, "spill_sum": cso_sum}, index=[now.strftime("%d/%m %H:%M")]
+    )
+    sim_result = pd.concat([cso_result, sum_result], axis=1)
+
+    display(sim_result)  # type: ignore
+    if EVENT_NAME[-1] == "Full year sim":
+        results = pd.read_csv(
+            "RTC/results/event_optimisation_full_result.csv", index_col=0
+        )
+        updated_results = pd.concat([results, sim_result])
+        updated_results.to_csv("RTC/results/event_optimisation_full_result.csv")
+    else:
+        # results = pd.read_csv("RTC/results/event_optimisation_full_result.csv", index_col=0)
+        # updated_results = pd.concat([results, sim_result])
+        sim_result.to_csv("RTC/results/event_optimisation_full_result.csv")
+print("DONE!")
 
 
 # x_vars[1] = cso_1 for timestep 1
