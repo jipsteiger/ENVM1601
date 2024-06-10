c79942dab409509bf5fd1ab15f20ad84571350ce
Jip Steiger <jipsteiger95@gmail.com>
2024-06-10 11:36:55 +0200

    RTC 53 90

--- RTC/heuristic_rules.py
+++ RTC/heuristic_rules.py
@@ -10,8 +10,6 @@ def CSO_Pump_21(nodes, target=None):
     """
     if nodes["j_21"].depth >= 1.4:
         target = 1
-    if (nodes["j_21"].depth >= 0.8) & (nodes["j_2"].depth >= 1.5):  # NOT NECCESARYY!?
-        target = 1
     if nodes["j_21"].depth <= 0.7:
         target = 0
     return "CSO_Pump_21", target
@@ -19,12 +17,7 @@ def CSO_Pump_21(nodes, target=None):
 
 def p_21_2(nodes, target=None):
     if nodes["j_21"].depth >= 0.25:
-        if nodes["j_2"].depth <= 0.5:
-            target = 1
-        elif (nodes["j_21"].depth >= 0.8) & (nodes["j_2"].depth <= 1.5):
-            target = 1
-        else:
-            target = 0
+        target = 1
     if nodes["j_21"].depth <= 0.10:
         target = 0
 
@@ -33,12 +26,7 @@ def p_21_2(nodes, target=None):
 
 def p_20_2(nodes, target=None):
     if nodes["j_20"].depth >= 0.25:
-        if nodes["j_2"].depth <= 0.5:
-            target = 1
-        elif nodes["j_20"].depth >= 1.7:
-            target = 1
-        else:
-            target = 0
+        target = 1
     if nodes["j_20"].depth <= 0.10:
         target = 0
     return "p_20_2", target
@@ -47,10 +35,6 @@ def p_20_2(nodes, target=None):
 def CSO_Pump_2(nodes, target=None):
     if nodes["j_2"].depth >= 2.1:
         target = 1
-    if (nodes["j_20"].depth >= 1.9) or (
-        nodes["j_21"].depth >= 1
-    ):  # Rather prioritise p_2_1
-        target = 1
     if nodes["j_2"].depth <= 1.5:
         target = 0
     return "CSO_Pump_2", target
@@ -58,35 +42,37 @@ def CSO_Pump_2(nodes, target=None):
 
 def p_2_1(nodes, target=None):
     if nodes["j_2"].depth >= 0.25:
-        if nodes["j_1"].depth <= 0.5:
-            target = 1
-        elif nodes["j_2"].depth >= 1.1:
-            target = 1
-        else:
-            target = 0
-
+        target = 1
+    elif (nodes["j_20"].depth >= 0.25) or \
+        (nodes["j_21"].depth >= 0.25):
+        target = 1
     if nodes["j_2"].depth <= 0.10:
         target = 0
     return "p_2_1", target
 
 
 def WWTP_inlet(nodes, target=None):
-    if nodes["j_1"].depth >= 0.25:
+    if (nodes["j_1"].depth >= 0.25) or \
+        (nodes["j_10"].depth >= 0.25) or \
+        (nodes["j_2"].depth >= 0.25) or \
+        (nodes["j_20"].depth >= 0.25) or \
+        (nodes["j_21"].depth >= 0.25):
         target = 1
-    if nodes["j_1"].depth <= 0.10:
+    else:
         target = 0
     return "WWTP_inlet", target
 
 
 def p10_1(nodes, target=None):
     if nodes["j_10"].depth >= 0.25:
-        if nodes["j_1"].depth <= 0.5:
-            target = 1
-        elif nodes["j_10"].depth >= 1.8:
-            target = 1
-        else:
+        if (nodes["j_2"].depth >= 0.25) or \
+            (nodes["j_20"].depth >= 0.25) or \
+            (nodes["j_21"].depth >= 0.25):
             target = 0
-
+        else:
+            target = 1
+        if nodes["j_10"].depth >= 1.8:
+            target = 1
     if nodes["j_10"].depth <= 0.10:
         target = 0
 
